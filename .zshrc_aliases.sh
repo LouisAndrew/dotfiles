6df435fa85310b4ddbbc8ddc97f3d667b1afdb6e
Louis Andrew <louisandrew3@gmail.com>
2022-03-22 14:12:33 +0100

    update

--- .zshrc_aliases.sh
+++ .zshrc_aliases.sh
@@ -1,11 +1,9 @@
-# export variable $ALIAS_FILE_PATH on your `.zshrc` file that's pointing to the absolute path of this file
+# export variable $ALIAS_FILE_PATH and $DOTFILES_PATH on your `.zshrc` file that's pointing to the absolute path of this file
 
 alias ll="ls -la"
 alias gh-cpr="gh pr create -w"
 alias gh-vpr="gh pr view -w"
 alias gh-lpr="gh pr list"
-
-alias zsh-config="code ~/.zshrc"
 
 alias pre="npm run lint:fix; npm t && g-cm"
 
@@ -20,19 +18,27 @@ alias g-c="git checkout"
 alias g-cb="g-c -b"
 alias g-pu="g-p -u origin \$(g-b)"
 alias g-a="git add *"
+alias g-n="echo `g-b` | sed  's/^.*\([0-9]\{4\}\).*$/\1/g'"
 
-alias cc="clear"
+alias clc="clear"
 
 alias pb-latest="npm i @plattenbau/component-library@latest"
 alias fs="fs-cli && cd \$(clipboard)"
 alias nrun="select-run"
 alias re-term="source ~/.zshrc"
 
+# `cd` to iCloud directory
 alias icl="cd ~/Library/Mobile\ Documents/com~apple~CloudDocs"
+alias icloud="icl"
 
 export VSCODE_SETTINGS_PATH="/Users/louis.andrew/Library/'Application Support'/Code/User/settings.json"
 
 alias vsc-config="code $VSCODE_SETTINGS_PATH"
+alias zsh-config="code ~/.zshrc"
+alias alias-config="code $ALIAS_FILE_PATH"
+alias dotfies="code $DOTFILES_PATH"
+alias hyper-config="code ~/.hyper.js"
+alias config="echo vsc-config alias-config dotfiles hyper-config"
 
 alias g-local="git config user.email louisandrew3@gmail.com"
 alias g-delete-origin="git push origin --delete"
@@ -42,3 +48,8 @@ alias dj-start='python ./boilerplate/manage.py runserver'
 alias alias:custom="cat $ALIAS_FILE_PATH"
 alias vsc-to-dotfiles="cat $VSCODE_SETTINGS_PATH >> $DOTFILES_PATH/.vscode/settings.json"
 alias backup:vsc="vsc-to-dotfiles && $DOTFILES_PATH && git add .vscode && g-cm 'chore: backup vscode config' && g-pu"
+
+# Watch vue tests
+alias tvw="npx vue-cli-service test:unit --watch"
+
+export SPEC="spec.ts"
