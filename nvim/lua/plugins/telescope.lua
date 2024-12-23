6104b4d10799364011da8b4f6fb1e6c7b8ad810d
Louis <louisandrew3@gmail.com>
2024-12-23 18:42:28 +0100

    feat(telescope): add floating backdrop and custom padded borders

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -85,18 +85,23 @@ return {
 
 		telescope.setup({
 			defaults = {
-				border = true,
+				-- border = "single",
 				mappings = default_maps,
 				path_display = path_display,
 				layout_strategy = "horizontal",
+				borderchars = {
+					prompt = { "▀", "▐", "▄", "▌", "▛", "▜", "▟", "▙" },
+					results = { "▀", "▐", "▄", "▌", "▛", "▜", "▟", "▙" },
+					preview = { "▀", "▐", "▄", "▌", "▛", "▜", "▟", "▙" },
+				},
 				layout_config = {
 					horizontal = {
-						width = {
-							padding = 0,
-						},
-						height = {
-							padding = 0,
-						},
+						-- width = {
+						-- 	padding = 0,
+						-- },
+						-- height = {
+						-- 	padding = 0,
+						-- },
 						preview_width = 0.7,
 					},
 				},
@@ -147,6 +152,47 @@ return {
 		telescope.load_extension("undo")
 		telescope.load_extension("noice")
 
+		-- Telescope backdrop. https://github.com/nvim-telescope/telescope.nvim/issues/3020
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "TelescopePrompt",
+			callback = function(ctx)
+				local backdropName = "TelescopeBackdrop"
+				local telescopeBufnr = ctx.buf
+				local telescopeZindex = 50
+
+				local backdropBufnr = vim.api.nvim_create_buf(false, true)
+				local winnr = vim.api.nvim_open_win(backdropBufnr, false, {
+					relative = "editor",
+					row = 0,
+					col = 0,
+					width = vim.o.columns,
+					height = vim.o.lines,
+					focusable = false,
+					style = "minimal",
+					zindex = telescopeZindex - 1, -- ensure it's below the reference window
+				})
+
+				vim.api.nvim_set_hl(0, backdropName, { bg = "#000000", default = true })
+				vim.wo[winnr].winhighlight = "Normal:" .. backdropName
+				vim.wo[winnr].winblend = 50
+				vim.bo[backdropBufnr].buftype = "nofile"
+
+				vim.api.nvim_create_autocmd({ "WinClosed", "BufLeave" }, {
+					once = true,
+					buffer = telescopeBufnr,
+					callback = function()
+						if vim.api.nvim_win_is_valid(winnr) then
+							vim.api.nvim_win_close(winnr, true)
+						end
+
+						if vim.api.nvim_buf_is_valid(backdropBufnr) then
+							vim.api.nvim_buf_delete(backdropBufnr, { force = true })
+						end
+					end,
+				})
+			end,
+		})
+
 		vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 		vim.keymap.set("n", "<C-o>", builtin.find_files, {})
 		-- find siblings
@@ -170,5 +216,7 @@ return {
 		vim.keymap.set("n", "<leader>pn", function()
 			require("telescope.builtin").find_files({ cwd = vim.fn.stdpath("config") })
 		end, { desc = "Search for nvim-config" })
+
+		-- CONFIG
 	end,
 }
