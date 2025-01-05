49250344b6701b3e045b4211e1500449a3e6b5f4
Louis <louisandrew3@gmail.com>
2025-01-05 20:15:52 +0100

    feat(vimium): adjust vimium css file for vimium-C

--- zshconfig.sh
+++ zshconfig.sh
@@ -75,9 +75,9 @@ BG_COLOR_DARK="#0a0a0a"
 export JQ_COLORS="0;90:$JQ_PROP:$JQ_PROP:$JQ_PROP:$JQ_STRING:$JQ_OBJECT:$JQ_OBJECT:$JQ_OBJECT"
 
 export LS_COLORS=$LS_COLORS:"di=1;$COLOR_MAGENTA"; 
-export EZA_COLORS="sc=0;$COLOR_CYAN:di=0;30;45:bu=0;$COLOR_YELLOW"
-export MASON="/Users/louis.andrew/.local/share/nvim/mason/packages"
-export VOLAR_GLOBAL="/Users/louis.andrew/.local/share/nvim/mason/packages/vue-language-server/node_modules/@vue/language-server/out/index.js"
+export EZA_COLORS="sc=0;$COLOR_CYAN:di=1;$COLOR_CYAN:bu=0;$COLOR_YELLOW"
+export MASON="$HOME/.local/share/nvim/mason/packages"
+export VOLAR_GLOBAL="$HOME/.local/share/nvim/mason/packages/vue-language-server/node_modules/@vue/language-server/out/index.js"
 export EDITOR="nvim"
 export KUBE_EDITOR="$EDITOR"
 export NEOVIDE_FRAME="none"
