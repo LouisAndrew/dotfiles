256dc65afe535b45de9ac5dbf5a7b8eb6312fca0
Louis Andrew <louisandrew3@gmail.com>
2026-06-30 19:13:27 +0200

    feat: remove all nvim config

--- nvim/colors/minimal_fedu.lua
+++
@@ -1,4 +0,0 @@
-vim.g.colors_name = "minimal_fedu"
-
-package.loaded["lush_theme.minimal_fedu"] = nil
-require("lush")(require("lush_theme.minimal_fedu"))
--- nvim/colors/my-theme.lua
+++
@@ -1,8 +0,0 @@
-require("xeno").setup({
-  base = "#1E1E1E",
-  accent = "#8CBE8C",
-  variation = 0.0,
-  contrast = 0.1,
-  transparent = true,
-})
-vim.g.colors_name = "my-theme"
