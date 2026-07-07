2ac3885731219fd89c74d08487b5ef1efc257065
Louis Andrew <louisandrew3@gmail.com>
2026-07-07 14:18:48 +0200

    feat: add zsh plugin for nvim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -7,6 +7,10 @@ vim.keymap.set("n", "<c-f>", function()
   Snacks.picker.grep({})
 end, { desc = "Open grep" })
 
+vim.keymap.set("n", "<leader>th", "<cmd>tabnext<cr>", { desc = "Next tab" })
+vim.keymap.set("n", "<leader>tl", "<cmd>tabprevious<cr>", { desc = "Previous tab" })
+vim.keymap.set("n", "<leader>tq", "<cmd>tabclose<cr>", { desc = "Close tab" })
+
 -- for example `10<A-h>` will `resize_left` by `(10 * config.default_amount)`
 vim.keymap.set("n", "<A-h>", require("smart-splits").resize_left)
 vim.keymap.set("n", "<A-j>", require("smart-splits").resize_down)
--- shell/config/zsh.sh
+++ shell/config/zsh.sh
@@ -2,3 +2,15 @@ export EDITOR="nvim"
 export KUBE_EDITOR="$EDITOR"
 ZSH_HIGHLIGHT_STYLES[unknown-token]='fg=red'
 ZSH_HIGHLIGHT_STYLES[arg0]='fg=white'
+
+open-nvim-cwd() {
+  zle -I
+  nvim
+  zle reset-prompt
+}
+
+zle -N open-nvim-cwd
+bindkey '^O' open-nvim-cwd
+bindkey -M viins '^O' open-nvim-cwd
+bindkey -M vicmd '^O' open-nvim-cwd
+bindkey -M emacs '^O' open-nvim-cwd
